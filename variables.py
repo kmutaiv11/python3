8ebad8edc2c5935f36c63a2c72edb764117565ad
kmutaiv11 <83009978+kmutaiv11@users.noreply.github.com>
2021-09-06 10:35:23 +0300

    first commit

---
+++ variables.py
@@ -0,0 +1,43 @@
+##
+# Variables Lecture
+##
+
+# -*- coding: utf-8 -*-
+
+
+# 'one' is the name of the variable
+# '=' is called the assignment operator we use to store values in a variable
+# '1' is the actual value stored inside the variable 'one'
+#one = 1
+
+# 'two' is the name of the variable
+# '=' is called the assignment operator we use to store values in a variable
+# '2' is the actual value stored inside the variable 'two'
+#two = 2
+
+# 'three' is the name of the variable
+# '=' is called the assignment operator we use to store values in a variable
+# '3' is the actual value stored inside the variable 'three'
+#three = 3
+
+# 'print()' is a function we use in python to print some value or content on the screen
+# name of the variable can be passed inside the print function to display its value on screen
+#print(one)
+#print(two)
+#print(three)
+#print(two)
+#print(one)
+# text values also called strings can be stored inside a variable
+# notice how the value 'Hello' is stored inside a variable 'StringVar'
+#StringVar = "Hello world"
+#print(StringVar)
+#one , two , three = 1,2,3
+#print(one)
+#print(two)
+#print(three)
+# Declaraing a variable and storing the value 5 into it
+#Five = 3+2
+#print(Five)
+count =1
+count = count + 1
+print(count)
